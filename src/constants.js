9fb963d6d4b1e90beda77aa55da68758bee22119
Acers <karthik.kmk.krishna@gmail.com>
2025-02-26 09:45:22 +0530

    vite config

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:5000"
-// export const API_BASE_URL = "PUT NGROK LINK HERE"
+// export const API_BASE_URL = "http://localhost:5000"
+export const API_BASE_URL = " https://3108-49-37-241-155.ngrok-free.app"
 
 
 
